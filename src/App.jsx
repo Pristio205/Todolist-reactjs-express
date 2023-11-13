800c42d22661ce77edcc6b4724c732fffc3e5903
Pristio Medi <200401127@student.umri.ac.id>
2023-11-13 23:01:34 +0700

    resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {Route, Routes } from "react-router-dom";
 import TodoList from './components/TodoList';
-import Login from './components/login';
-import Register from './components/register';
+import Login from './components/Login';
+import Register from './components/Register';
 
 const App = () => {
   return (
